4796df75616b7729ed3d386348d295d1b5c2e075
JCWarmels <jesse.warmels@student.rocva.nl>
2020-03-02 10:02:54 +0100

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -10,36 +10,45 @@ $pdo = new PDO($dsn, $user, $passwd);
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title></title>
 </head>
+
 <body>
     <main>
         <h1>Welkom op het netland beheerderspaneel</h1>
         <h2>Series</h2>
         <table>
-        <tr><th style='width:150px'>Title</th><th style='width:150px'>Rating</th></tr>
-        <?php 
-        $result_series =  $pdo->query("SELECT title, rating FROM series ORDER BY rating DESC");
-        while($row_series = $result_series->fetch()) {
-            echo '<tr><td><a href="series.php?title='.$row_series['title'].'">';
-            echo($row_series['title'] . '</a></td><td style="text-align:center;">' .  $row_series['rating']);
-            echo '</td></tr>';
-        }
-        ?>
+            <tr>
+                <th style='width:150px'>Title</th>
+                <th style='width:150px'>Rating</th>
+            </tr>
+            <?php
+            $result_series =  $pdo->query("SELECT id, title, rating FROM series ORDER BY rating DESC");
+            while ($row_series = $result_series->fetch()) {
+                echo '<tr><td><a href="series.php?id=' . $row_series['id'] . '">';
+                echo ($row_series['title'] . '</a></td><td style="text-align:center;">' .  $row_series['rating']);
+                echo '</td></tr>';
+            }
+            ?>
         </table>
         <h2>Films</h2>
         <table>
-        <tr><th style='width:150px'>Title</th><th style='width:150px'>Duration</th></tr>
-        <?php 
-        $result_films =  $pdo->query("SELECT titel, duur_in_min FROM films");
-        while($row_films = $result_films->fetch()) {
-            echo '<tr><td><a href="films.php?title='.$row_films['titel'].'">';
-            echo($row_films['titel'] . '</td><td style="text-align:center;">' .  $row_films['duur_in_min']);
-            echo '</td></tr>';
-        }
-        ?>    
+            <tr>
+                <th style='width:150px'>Title</th>
+                <th style='width:150px'>Duration</th>
+            </tr>
+            <?php
+            $result_films =  $pdo->query("SELECT volgnummer, titel, duur_in_min FROM films");
+            while ($row_films = $result_films->fetch()) {
+                echo '<tr><td><a href="films.php?id=' . $row_films['volgnummer'] . '">';
+                echo ($row_films['titel'] . '</td><td style="text-align:center;">' .  $row_films['duur_in_min']);
+                echo '</td></tr>';
+            }
+            ?>
         </table>
     </main>
 </body>
+
 </html>
--- series.php
+++ series.php
@@ -5,39 +5,38 @@ $user = "root";
 $passwd = "";
 
 $pdo = new PDO($dsn, $user, $passwd);
-
+$series_request = $pdo->prepare("SELECT * FROM series WHERE id=?");
+$series_request->execute([$_GET['id']]);
+$to_show = $series_request->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title></title>
 </head>
+
 <body>
     <main>
         <h1>Welkom op het netland beheerderspaneel</h1>
-        <h2>Hier vindt u all data over<?php echo PHP_EOL . $_GET['title']; ?>:</h2>
-        <?php
-
-        $series_request = $pdo->prepare("SELECT * FROM series WHERE title=?");
-        $series_request->execute([$_GET['title']]);
-        $to_show = $series_request->fetch(PDO::FETCH_ASSOC);
-        ?>
+        <h2>Hier vindt u all data over<?php echo PHP_EOL . $to_show['title']; ?>:</h2>
         <div style="display:flex; flex-direction:row; width:150px; justify-content: space-around;">
-            <h3><?php echo $to_show['title']?></h3>
-            <h4><?php echo $to_show['rating']?>/5.0</h4>
+            <h3><?php echo $to_show['title'] ?></h3>
+            <h4><?php echo $to_show['rating'] ?>/5.0</h4>
         </div>
         <div style="display:flex; flex-direction:row; width:150px; justify-content: space-around;">
-            <h4><?php echo $to_show['has_won_awards'].PHP_EOL?>awards won</h4>
-            <h4><?php echo $to_show['seasons'].PHP_EOL?>seasons</h4>
+            <h4><?php echo $to_show['has_won_awards'] . PHP_EOL ?>awards won</h4>
+            <h4><?php echo $to_show['seasons'] . PHP_EOL ?>seasons</h4>
         </div>
         <div style="display:flex; flex-direction:row; width:150px; justify-content: space-around;">
-            <h4><?php echo $to_show['language'].PHP_EOL?></h4>
-            <h4><?php echo $to_show['country'].PHP_EOL?></h4>
+            <h4><?php echo $to_show['language'] . PHP_EOL ?></h4>
+            <h4><?php echo $to_show['country'] . PHP_EOL ?></h4>
         </div>
         <div style="display:flex; flex-direction:row; width:800px;">
-            <p style="width:350px;"><?php echo $to_show['description']?></p>
+            <p style="width:350px;"><?php echo $to_show['description'] ?></p>
         </div>
     </main>
 </body>
+
 </html>
